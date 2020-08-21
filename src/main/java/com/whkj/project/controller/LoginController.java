4202457ed61ab94c6ca0b82c5fc2ce8bc6b4820c
wuliqun <wuliqun@qq.com>
2020-08-21 16:18:46 +0800

    view.html

--- src/main/java/com/whkj/project/controller/LoginController.java
+++ src/main/java/com/whkj/project/controller/LoginController.java
@@ -1,12 +1,17 @@
 package com.whkj.project.controller;
 
 import com.whkj.project.utils.MD5Util;
+import com.whkj.project.utils.RestResult;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Objects;
 
 
 @Controller
@@ -19,11 +24,16 @@ public class LoginController {
 
 
     @PostMapping(value = "/login")
-    public String login(String username,String password){
+    public String login(String username, String password){
         Subject subject = SecurityUtils.getSubject();
         password = MD5Util.encrypt(username.toLowerCase(), password);
         UsernamePasswordToken token = new UsernamePasswordToken(username, password);
         subject.login(token);
         return "success";
+    }
+
+    @GetMapping(value = "/index")
+    public String index(){
+        return "blog/blog_admin/index";
     }
 }
